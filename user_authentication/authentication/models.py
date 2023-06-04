2c8ff881be5c9eafcdea55905af23aef96d1e7ce
Parthiv Parmar <parmarparthiv77@gmail.com>
2023-06-04 17:21:33 +0530

    Add files via upload

---
+++ user_authentication/authentication/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.TextField(max_length=15)
+    last_login = models.DateTimeField(auto_now=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+
+    def __str__(self):
+        return self.email
---
+++ user_authentication/authentication/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth import authenticate,login
+from .models import User
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if confirm_password != password:
+            return HttpResponse('Passwords do not match')
+        else:
+            user = User.objects.create_user(email=email, password=password, first_name=first_name, last_name=last_name, username=username)
+            messages.success(request, 'Account created successfully.')
+            return redirect('login')
+
+    return render(request, 'register.html')
+
+def logIn(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid email or password.')
+            return redirect('login')
+    
+    return render(request, 'login.html')
+
+
+def home(request):
+    return render(request,'home.html')
+
+def platform(request):
+    return render(request,'platforms.html')
+
+
